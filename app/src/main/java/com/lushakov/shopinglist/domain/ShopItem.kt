853bbb51ae7371ca8100518a70321fbdc4591318
Иван Лушаков <ivl@m4bank.com>
2022-02-16 14:15:04 +0300

    Added domain- and data- layer

--- app/src/main/java/com/lushakov/shopinglist/domain/ShopItem.kt
+++ app/src/main/java/com/lushakov/shopinglist/domain/ShopItem.kt
@@ -1,3 +1,8 @@
 package com.lushakov.shopinglist.domain
 
-data class ShopItem( val id: Int, val name: String, val count: Int, val enabled: Boolean)
+data class ShopItem(val name: String, val count: Int, val enabled: Boolean, var id: Int = UNDEFINED_ID)
+{
+    companion object{
+        const val UNDEFINED_ID = -1
+    }
+}
